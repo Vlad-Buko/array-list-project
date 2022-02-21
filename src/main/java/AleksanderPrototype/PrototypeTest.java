cf79c43ccda8151b0cecb6a9878bd2d4cb2b49f5
Sasa537 <sasa5375632@gmail.com>
2022-02-21 22:52:04 +0300

    Исправил замечания сонара

--- src/main/java/AleksanderPrototype/PrototypeTest.java
+++ src/main/java/AleksanderPrototype/PrototypeTest.java
@@ -4,5 +4,6 @@ public class PrototypeTest {
     public static void main(String[] args) {
         ComplicatedObject prototype = new ComplicatedObject("Example");
         ComplicatedObject clone = (ComplicatedObject) prototype.copy();
+
     }
 }
